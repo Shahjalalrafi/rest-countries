47ba5f19130b0017eda5bbe5611be2b8fb4417b3
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-02-08 16:40:00 +0300

    upload

--- index.js
+++ index.js
@@ -1,47 +1,26 @@
-// const details = 'https://restcountries.eu/rest/v2/all'
-// const list = document.querySelector('.my-list')
-// let para = document.getElementById('para')
-
-// function data() {
-//     fetch(details)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data)
-//             let country = data[3].name
-//             let crt = document.createElement('li')
-//             crt.innerText = country
-
-//             list.appendChild(crt).addEventListener('click', function() {
-//                  para.innerText = data[3].subregion
-//                  const img = document.createElement('img')
-//                  img.setAttribute('src', data[3].flag)
-//                  img.style.width = 200+ 'px'
-//                  para.appendChild(img)
-//             })
-
-//         })
-// }
-
-// data()
-
-
-let container = document.querySelector('.container')
 fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data => displayData(data))
+.then(res => res.json())
+.then(data => displayData(data))
 
-function displayData(countries) {
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i]
-        const div = document.createElement('div')
-        const heading = document.createElement('h1')
-        const p = document.createElement('p')
-        const countryName = country.name
-        const countryCapital = country.capital
-        heading.innerHTML = countryName
-        p.innerHTML = countryCapital
-        div.appendChild(heading)
-        div.appendChild(p)
-        container.appendChild(div)
-    }
+const displayData = countries => {
+    const countriesDiv = document.querySelector('.container')
+
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div')
+        countryDiv.className = 'countryDiv'
+        const countryInfo = `
+            <h3>${country.name}</h3>
+        `
+        
+        countryDiv.innerHTML = countryInfo
+        countryDiv.addEventListener('click', function() {
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <p>${country.capital}</p>
+            <img src="${country.flag}">
+            `
+            countryDiv.appendChild(div)
+        })
+        countriesDiv.appendChild(countryDiv)
+    })
 }
